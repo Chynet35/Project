09af7eeb612ad542dc047465527cf5f43525ca49
Chioma Enemuo <enemuochioma77@gmail.com>
2023-06-11 21:14:02 +0100

    Update

--- static/js/script .js
+++ static/js/script .js
@@ -1,3 +1,16 @@
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const apiUrl = "https://api.quotable.io/random"
+
+async function getQuote(url){
+    const response = await fetch(url)
+    let data = await response.json()
+    quote.innerHTML = data.content
+    author.innerHTML = data.author
+}
+
+getQuote(apiUrl)
+
 // Function to generate random quote
 function generateQuote() {
     //Make an AJAX request to the server
